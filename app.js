cd4c60c89efc4c744b3bd3d32714cc934b75faf3
Stephanie Baron <stephaniebaron12@gmail.com>
2017-11-16 10:17:14 -0500

    Sorted strings alphabetically

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var array2 = [ "lion", "aardvark", "gorilla" ];
 function mergeSortedArrs(a,b){
   for (var i = 0; i < b.length; i++){
       a.push(b[i]);
+      var animalArr = a;
   }
-  return a;
+  return animalArr.sort();
 }
 
 mergeSortedArrs(array1, array2);
